cf9b598451fcab31be0d28100a0ca9519b146f49
Reeces Piecces <rtwboss@gmail.com>
2025-04-05 19:09:48 -0400

    Set Basic structure for Report page

--- app/src/pages/ReportPage.tsx
+++ app/src/pages/ReportPage.tsx
@@ -1,8 +1,27 @@
+import Navbar from "@/components/ui/Navbar"
+
 function ReportPage() {
-    return(
-        <div id="Analysis">
-            <h1>Analysis</h1>
-            <p id="data"></p>
+    return (
+        <div>
+            <Navbar/>
+            <main className="flex flex-grow items-center justify-center px-4 md:px-8 m-10">
+                <div id="Analysis" >
+                    {/*Heading*/}
+                    <h1 className="text-[5rem] text-center font-bold text-blue-800 font-theme tracking-wide mb-2 ">
+                        Analysis
+                    </h1>
+
+                    {/*Graph*/}
+                    <img>
+                    
+                    </img>
+
+                    {/*Text Report*/}
+                    <p id="data" className="bg-gray-100 p-5"> 
+
+                    </p>
+                </div>
+            </main>
         </div>
     )
 }
