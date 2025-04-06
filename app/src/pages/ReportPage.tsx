535da1a9df1e456775b99314c54a792b1a7209f8
Reeces Piecces <rtwboss@gmail.com>
2025-04-06 03:22:04 -0400

    Report Page up to date

--- app/src/pages/ReportPage.tsx
+++ app/src/pages/ReportPage.tsx
@@ -1,10 +1,15 @@
 import Navbar from "@/components/ui/Navbar"
+import ParticleComponent from "@/components/ui/Particles"
 
 function ReportPage() {
     return (
-        <div>
-            <Navbar/>
-            <main className="flex flex-grow items-center justify-center px-4 md:px-8 m-10 mt-30">
+        <div className="flex flex-col bg-white text-blue-900 min-h-screen w-full">
+            <Navbar />
+            {/*Particles*/}
+            <div className="absolute inset-0 z-0 blur-xs">
+                <ParticleComponent />
+            </div>
+            <main className="flex flex-grow items-center justify-center px-4 z-20">
                 <div id="Analysis" >
                     {/*Heading*/}
                     <h1 className="text-[5rem] text-center font-bold text-blue-800 font-theme tracking-wide mb-2 ">
@@ -12,14 +17,40 @@ function ReportPage() {
                     </h1>
 
                     {/*Graph*/}
-                    <img>
-                    
-                    </img>
+                    <img src=""/>
 
                     {/*Text Report*/}
-                    <p id="data" className="bg-gray-100 p-5"> 
+                    <p id="data" className="bg-gray-100 p-5">
 
                     </p>
+
+                    {/* Table*/}
+                    <table>
+                        {/*Row1*/}
+                        <tr>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                        {/*Row2*/}
+                        <tr>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                        {/*Row3*/}
+                        <tr>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                        {/*Row4*/}
+                        <tr>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </table>
                 </div>
             </main>
         </div>
